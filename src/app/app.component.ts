4d718d57ec3e1e27d3a01ab3a22b62908f948bb9
maradasankarrao <maradasankar3@gmail.com>
2019-12-10 11:45:44 +0530

    displaying fruits

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+fruitname:string=""
+fruitprice:number=0
+fruite=[]
+additem(){
+  this.fruite.push(this.fruitname)
+
+}
+}
